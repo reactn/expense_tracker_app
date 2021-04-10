0bde7790ba5fcfd3bb7b0b14413a30eadd5684d2
reactn <reactjs1989@gmail.com>
2021-04-10 06:37:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow } = require('electron')
+
+require('@electron/remote/main').initialize()
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    })
+
+    win.loadURL('http://localhost:3000')
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function() {
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+})
